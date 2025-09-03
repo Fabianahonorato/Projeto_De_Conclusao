bae271338edda689866a9360b63c741b1d51763f
Fabianahonorato <fho@discente.ifpe.edu.br>
2025-09-03 20:29:25 -0300

    atualização projeto categoria

--- src/pessoal/modelo/Categoria.java
+++ src/pessoal/modelo/Categoria.java
@@ -1,5 +1,25 @@
 package pessoal.modelo;
 
 public class Categoria {
-
+   private String nome;
+	
+   public Categoria(String nome) {
+	   this.nome = nome;
+   }
+	
+   public String getNome() {
+	   return nome;
+   }
+   
+   public void stNome(String nome) {
+	   this.nome = nome;
+	   
+   }
+   
+   @Override
+   public String toString() {
+	   return nome;
+	   
+   }
 }
+
