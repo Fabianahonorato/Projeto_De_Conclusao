ac65ff88b1baa826790f78a8b61eda864eb79293
Fabianahonorato <fho@discente.ifpe.edu.br>
2025-09-03 21:35:35 -0300

    atualização da classe Item

--- src/pessoal/modelo/Livro.java
+++ src/pessoal/modelo/Livro.java
@@ -1,5 +1,28 @@
 package pessoal.modelo;
 
-public class Livro {
-
+public class Livro extends Item {
+   private String isbn;
+   private int anoPublicacao;
+   
+   public Livro(String titulo, String autorArtista, Categoria categoria, String isbn, int anoPublicado) {
+	   super(titulo, autorArtista, categoria);
+	   this.isbn = isbn;
+	   this.anoPublicacao = anoPublicacao;
+	   
+   }
+   
+   public String getIsbn() {
+	   return isbn;
+   }
+  
+   @Override
+   public String getTipo() {
+	   return "Livro";
+   }
+   @Override 
+   public String toString() {
+	  return super.toString() + ", Tipo: Livro, ISBN: " + isbn + ", Ano: " + anoPublicacao;
+  }
 }
+
+
