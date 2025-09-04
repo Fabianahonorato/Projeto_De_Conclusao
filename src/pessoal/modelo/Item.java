cbe131453ca157b912eaad3d44250bfb28d93485
Fabianahonorato <fho@discente.ifpe.edu.br>
2025-09-03 21:06:39 -0300

    atualização da classe item

--- src/pessoal/modelo/Item.java
+++ src/pessoal/modelo/Item.java
@@ -1,5 +1,44 @@
 package pessoal.modelo;
 
-public class Item {
+public abstract class Item {
+  private String titulo;
+  private String autorArtista;
+  private Categoria categoria;
+  
+  public Item(String titulo, String autorArtista, Categoria categoria) {
+	 this.titulo = titulo;
+	 this.autorArtista = autorArtista;
+	 this.categoria = categoria;
+	  
+  }
+	
+  public String getTitulo() {
+	  return titulo;
 
+ }
+  
+  public void setTitulo(String titulo) {
+	  this.titulo = titulo;
+	  
+  }
+  
+  public String getAutorArtista() {
+	  return autorArtista;
+  }
+  
+  public Categoria getCategoria() {
+      return categoria;
+
+  }
+   
+  public void steCategoria(Categoria categoria) {
+	  this.categoria = categoria;
+  }
+  
+  public abstract String getTipo();
+  
+      @Override
+      public String toString() {
+      return "Título: " + titulo + ", Autor/Artista: " + autorArtista + ", Categoria: " + categoria.getNome();
+      }
 }
