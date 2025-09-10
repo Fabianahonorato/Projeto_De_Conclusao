eef509c0cc38204e60c53950ed3889546babd7b5
Fabianahonorato <fho@discente.ifpe.edu.br>
2025-09-10 18:06:39 -0300

    atualização do Item Repository

--- src/pessoal/repositorio/ItemRepositoryImpl.java
+++ src/pessoal/repositorio/ItemRepositoryImpl.java
@@ -1,5 +1,33 @@
 package pessoal.repositorio;
 
-public class ItemRepositoryImpl {
+import pessoal.modelo.Item;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+
+public class ItemRepositoryImpl implements ItemRepository {
+	private List<Item> itens;
+
+    public ItemReposityImpl() {
+    	this.itens = new ArrayList<>();
+    	
+    }
+
+    @Override 
+    public void adicionarItem(Item item) {
+    	this.itens.add(item);
+    }
+
+    @Override 
+    public List<Item> listarTodosItens(){
+    	return new ArrayList<>(this.itens);
+    	
+    }
+    
+    @Override
+    public boolean removerItem(String titulo) {
+    	return this.itens.removeIf(item -> item.getTitulo(). equalsIgnoreCase(titulo));
+    }
 }
+
