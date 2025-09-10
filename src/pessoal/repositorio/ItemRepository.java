e4f6dbeacd7f857f8ecd68c32d44b4d10ad7ab73
Fabianahonorato <fho@discente.ifpe.edu.br>
2025-09-10 17:49:47 -0300

    atualização da interface epositório

--- src/pessoal/repositorio/ItemRepository.java
+++ src/pessoal/repositorio/ItemRepository.java
@@ -1,5 +1,17 @@
 package pessoal.repositorio;
 
-public class ItemRepository {
+import pessoal.modelo.Item;
+import java.util.List;
 
+public interface ItemRepository {
+  void adicionarItem(Item item);
+  List<Item> listarTodosItens();
+  List<Item> listarItensPorCategoria(String categoria);
+  boolean removerItem(String titulo);
+	
+	
+	
+	
+	
+	
 }
